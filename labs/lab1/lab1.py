373019068c043401a4dc0df6e12ed0963fe26acc
mastaidan <theaidanmast@gmail.com>
2022-01-20 17:46:45 -0500

    Completed Lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -4,8 +4,10 @@ Lab1.py
 Calculate monthly interest based on asked inputs
 I certify that this assignment is entirely my own work.
 """
+
+
 def monthly_interest():
-    monthly_percent_int = eval(input("What is the annual interest?"))/1200
+    monthly_percent_int = eval(input("What is the annual interest?")) / 1200
     billing_cycle_length = eval(input("How long is billing cycle?"))
     net_balance = eval(input("What is the net balance?"))
     payment = eval(input("What was your payment?"))
@@ -14,4 +16,4 @@ def monthly_interest():
     step_2 = payment * (billing_cycle_length - billing_day)
     average_daily_balance = (step_1 - step_2) / billing_cycle_length
     monthly_interest = average_daily_balance * monthly_percent_int
-    print("Your monthly interest is",monthly_interest,"!")
+    print("Your monthly interest is", monthly_interest, "!")
