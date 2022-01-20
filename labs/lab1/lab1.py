35b5c8e42515099a5d30ba886804e277ccc13823
mastaidan <theaidanmast@gmail.com>
2022-01-20 17:52:37 -0500

    Completed Lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -17,3 +17,6 @@ def monthly_interest():
     average_daily_balance = (step_1 - step_2) / billing_cycle_length
     monthly_interest = average_daily_balance * monthly_percent_int
     print("Your monthly interest is", monthly_interest, "!")
+    print("If you would like to calculate another monthly interest, then type monthly_interest().")
+
+monthly_interest()
