0c9923981bad46a7d29e908881737ce8c53c204a
mastaidan <theaidanmast@gmail.com>
2022-01-20 17:33:44 -0500

    Completed Lab 1

--- labs/lab1/lab1.py
+++ labs/lab1/lab1.py
@@ -5,14 +5,16 @@ Calculate monthly interest based on asked inputs
 I certify that this assignment is entirely my own work.
 """
 def monthly_interest():
-    monthly_percent_int = eval(input("gimme annual int please"))/1200
-    billing_cycle_length = eval(input("how long is billing cycle please"))
-    net_balance = eval(input("gimme net balance please"))
-    payment = eval(input("gimme payment please"))
-    billing_day = eval(input("gimme billing day please"))
+    monthly_percent_int = eval(input("What is the annual interest?"))/1200
+    billing_cycle_length = eval(input("How long is billing cycle?"))
+    net_balance = eval(input("What is the net balance?"))
+    payment = eval(input("What was your payment?"))
+    billing_day = eval(input("What day of the month did you pay on?"))
     step_1 = net_balance * billing_cycle_length
     step_2 = payment * (billing_cycle_length - billing_day)
     average_daily_balance = (step_1 - step_2) / billing_cycle_length
     monthly_interest = average_daily_balance * monthly_percent_int
-    print("your monthly interest is", monthly_interest,"!")
+    print("Your monthly interest is",monthly_interest,"!")
 
+def L():
+    print("L")
