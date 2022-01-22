7739a1f72c92184f334212bfe139b0b9393ea8b1
mastaidan <theaidanmast@gmail.com>
2022-01-21 22:30:00 -0500

    Completed HW 1

--- assignments/hw1/hw1.py
+++ assignments/hw1/hw1.py
@@ -1,34 +1,42 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Aidan Mast
+hw1.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Use functions to run the numbers.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
 
 
 def calc_rec_area():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    area = length * width
+    print("Area =", area)
 
 def calc_volume():
-    pass
-
+    length = eval(input("Enter the length: "))
+    width = eval(input("Enter the width: "))
+    height = eval(input("Enter the height"))
+    volume = length * width * height
+    print("Area =", volume)
 
 def shooting_percentage():
-    pass
-
+    total_shots = eval(input("Enter the player's total shots: "))
+    shots_made = eval(input("Enter how many shots the player made: "))
+    shot_percent = shots_made / total_shots * 100
+    print("Shooting Percentage:", shot_percent)
 
 def coffee():
-    pass
-
+    cof_lb = eval(input("How many pounds of coffee would you like?"))
+    total = round(cof_lb * 11.36 + 1.50, 2)
+    print("Your total is: $", total)
 
 def kilometers_to_miles():
-    pass
+    kilo = eval(input("How many kilometers did you travel?"))
+    mile = kilo / 1.61
+    print("That's", mile, ("miles!"))
 
 
 if __name__ == '__main__':
