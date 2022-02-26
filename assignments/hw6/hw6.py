2e139fd155f930a347413577f9a02ad29f60f694
mastaidan <theaidanmast@gmail.com>
2022-02-25 22:26:04 -0500

    Completed HW 6

--- assignments/hw6/hw6.py
+++ assignments/hw6/hw6.py
@@ -1,42 +1,81 @@
 """
-Name: <your name goes here – first and last>
-<ProgramName>.py
+Name: Aidan Mast
+hw6.py
 
-Problem: <Brief, one or two sentence description of the problem that this program solves, in your own words.>
+Problem: Create functions with parameters and encode stuff.
 
 Certification of Authenticity:
-<include one of the following>
 I certify that this assignment is entirely my own work.
-I certify that this assignment is my own work, but I discussed it with: <Name(s)>
 """
+from math import pi
 
 
 def cash_converter():
-    pass
+    amount = eval(input("enter an integer:"))
+    print("That is ${}.00".format(amount))
 
 
 def encode():
-    pass
+    message = input("enter a message:")
+    key = eval(input("enter a key:"))
+    message_length = len(message)
+    uni_message = []
+    coded_message = []
+    for index in range(message_length):
+        coded_value = ord(message[index]) + key
+        uni_message.append(coded_value)
+    for index in range(message_length):
+        coded_chr = chr(uni_message[index])
+        coded_message.append(coded_chr)
+    print("".join(coded_message))
 
 
 def sphere_area(radius):
-    pass
+    return 4 * pi * radius * radius
 
 
 def sphere_volume(radius):
-    pass
+    return 4 / 3 * pi * radius ** 3
 
 
 def sum_n(number):
-    pass
+    total = 0
+    for i in range(number + 1):
+        total = total + i
+    return total
 
 
 def sum_n_cubes(number):
-    pass
+    total = 0
+    for i in range(number + 1):
+        total = total + pow(i, 3)
+    return total
 
 
 def encode_better():
-    pass
+    org_message = input("enter a message:")
+    org_key = input("enter a key:")
+    message_length = len(org_message)
+    key_length = len(org_key)
+    number_message = []
+    number_key = []
+    coded_ord_message = []
+    coded_message = []
+    for index in range(message_length):
+        number_letter = ord(org_message[index]) - 65
+        number_message.append(number_letter)
+    for index in range(key_length):
+        number_letter = ord(org_key[index]) - 65
+        number_key.append(number_letter)
+    for i in range(message_length):
+        repeat_key_values = i % len(org_key)
+        coded_number = number_message[i] + number_key[repeat_key_values]
+        coded_ord_message.append(coded_number)
+    for i in range(message_length):
+        number = coded_ord_message[i] % 58
+        letter = chr(number + 65)
+        coded_message.append(letter)
+    print("".join(coded_message))
 
 
 if __name__ == '__main__':
